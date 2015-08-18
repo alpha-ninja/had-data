5999907120d6e27dd98495b512228ec7c685a15f
alpha-ninja <alpha_ninja@icloud.com>
2015-08-17 22:56:47 -0700

    script is done
    commit message courtesy of https://hackaday.io/hacker/18962

--- had.py
+++ had.py
@@ -20,13 +20,51 @@ print projects
 
 if download:
 	for project in projects:
+		print str(round((1.0 * projects.index(project)) / len(projects) * 10000) / 100)  + "%"
 		url = 'http://www.hackaday.io/project/' + str(project)
 		f2 = open('had/' + str(project) + '.txt', 'w')
 		response = urllib2.urlopen(url)
 		html = response.read()
 		f2.write(html)
 		f2.close()
-		print url
+		#print url
+
+		url = 'http://www.hackaday.io/project/' + str(project) + '/logs'
+		f2 = open('had/' + str(project) + 'L.txt', 'w')
+		response = urllib2.urlopen(url)
+		html = response.read()
+		f2.write(html)
+		f2.close()
+		#print url
+
+quarterfinalsMentions = 0
+
+for project in projects:
+	quarterfinalsMentioned = False
+	f2 = open('had/' + str(project) + '.txt', 'r')
+	projectraw = f2.read()
+	f2.close()
+	youtubes = re.findall(r'[\'"]https?://www.youtube\.com.*?</a>', projectraw, re.IGNORECASE)
+	print project
+	for youtube in youtubes:
+		if re.search(r'quarterfinal', youtube, re.IGNORECASE):
+			quarterfinalsMentioned = True
+		print "    " + youtube
+
+	f2 = open('had/' + str(project) + 'L.txt', 'r')
+	projectraw = f2.read()
+	f2.close()
+	youtubes = re.findall(r'[\'"]https?://www.youtube\.com.*?</a>', projectraw, re.IGNORECASE)
+	print project
+	for youtube in youtubes:
+		if re.search(r'quarterfinal', youtube, re.IGNORECASE):
+			quarterfinalsMentioned = True
+		print "    " + youtube
+
+	if quarterfinalsMentioned:
+		quarterfinalsMentions += 1
+
+print "Mentions of 'quarterfinal[s]': " + str(quarterfinalsMentions)
 
 '''
 
