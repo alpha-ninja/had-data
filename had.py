a61e8ec2288301f9a1c51721f723a9a2b3031f01
alpha-ninja <alpha_ninja@icloud.com>
2015-08-18 01:18:26 -0700

    I'll make this code less bad later™
    Seriously though, this code needs cleanup.

--- had.py
+++ had.py
@@ -40,29 +40,13 @@ if download:
 projectVideos = 0
 tally = {0: 0}
 projectsMin4Logs = 0
+projectsMin8Logs = 0
 projectsQualifying = list()
+projectsQualifyingBP = list()
 for project in projects:
-	projectVideo = False
-	project4Logs = False
-	f2 = open('had/' + str(project) + '.txt', 'r')
-	projectraw = f2.read()
-	f2.close()
-	youtubes = re.findall(r'["\'](?:https://|http://|//|)(?:www\.|)youtube\.[a-z\.]{2,6}/.*?["\']', projectraw, re.IGNORECASE)
-	#print project
-	for youtube in youtubes:
-		projectVideo = True
-		#print "    " + youtube
-
-	f2 = open('had/' + str(project) + 'L.txt', 'r')
-	projectraw = f2.read()
-	f2.close()
-	youtubes = re.findall(r'["\'](?:https://|http://|//|)(?:www\.|)youtube\.[a-z\.]{2,6}/.*?["\']', projectraw, re.IGNORECASE)
-	for youtube in youtubes:
-		projectVideo = True
-		#print "    " + youtube
-
-	if projectVideo is True:
-		projectVideos += 1
+	projectLogAmount = 0
+	projectVideo = 0
+	projectMentionsLicense = False
 
 
 
@@ -70,32 +54,52 @@ for project in projects:
 	f2 = open('had/' + str(project) + '.txt', 'r')
 	projectraw = f2.read()
 	f2.close()
+	licenses = re.findall(r'licen[sc]e', projectraw, re.IGNORECASE)
+	youtubes = re.findall(r'["\'](?:https://|http://|//|)(?:www\.|)youtu(?:be|)\.[a-z\.]{2,6}/.*?["\']', projectraw, re.IGNORECASE)
+	product = re.findall(r'<li><a href="/submissions/bestproduct/list">Best Product</a></li>', projectraw, re.IGNORECASE)
 	logamt = re.search(r'View all (\d*) project logs', projectraw, re.IGNORECASE)
 	if logamt is not None:
 		#print logamt.group(0)
 		if not int(logamt.group(1)) in tally.keys():
 			tally[int(logamt.group(1))] = 0
 		tally[int(logamt.group(1))] += 1
-		if int(logamt.group(1)) >= 4:
-			projectsMin4Logs += 1
-			project4Logs = True
+		projectLogAmount = int(logamt.group(1))
 	else:
 		if not "0" in tally.keys():
 			tally["0"] = 0
 		tally["0"] += 1
-	if project4Logs and projectVideo:
+		projectLogAmount = 0
+
+
+
+
+	f2 = open('had/' + str(project) + 'L.txt', 'r')
+	projectraw = f2.read()
+	f2.close()
+	licenses = list(licenses) + list(re.findall(r'licen[sc]e', projectraw, re.IGNORECASE))
+	youtubes = list(youtubes) + list(re.findall(r'["\'](?:https://|http://|//|)(?:www\.|)youtu(?:be|)\.[a-z\.]{2,6}/.*?["\']', projectraw, re.IGNORECASE))
+
+
+
+
+	if (projectLogAmount >= 4) and (len(youtubes) >= 1):
 		projectsQualifying.append(project)
+	if (projectLogAmount >= 8) and (len(youtubes) >= 2) and (len(licenses) > 0) and (len(product) > 0):
+		projectsQualifyingBP.append(project)
 		#print "Q: http://hackaday.io/project/" + str(project)
 
 
-print "Projects with at least one youtube video: " + str(projectVideos)
-print "Projects with X project logs:"
-pprint.pprint(tally)
-print "Projects with 4 or more project logs:"
-print projectsMin4Logs
+#print "Projects with at least one youtube video: " + str(projectVideos)
+#print "Projects with X project logs:"
+#pprint.pprint(tally)
+#print "Projects with 4 or more project logs:"
+#print projectsMin4Logs
 print "Projects QUALIFYING - with at least one youtube video and at least 4 logs:"
 print len(projectsQualifying)
 print projectsQualifying
+print "Projects QUALIFYING BP - with at least 2 youtube videos and at least 8 logs, mentioning 'license':"
+print len(projectsQualifyingBP)
+print projectsQualifyingBP
 '''
 
 --> projects 46 and 37 NO!
